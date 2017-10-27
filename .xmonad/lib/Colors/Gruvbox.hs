424f4c5b3845cf7db2069388ebcfcc41d6c22397
Brian Buccola <brian.buccola@gmail.com>
2017-10-27 10:16:12 +0200

    Add Gruvbox.hs

---
+++ .xmonad/lib/Colors/Gruvbox.hs
@@ -0,0 +1,29 @@
+module Colors.Gruvbox where
+
+-- =========================
+--  Gruvbox-inspired colors
+-- =========================
+
+myDarkBlack    = "#000000"
+myLightBlack   = "#928374"
+
+myDarkRed      = "#cc241d"
+myLightRed     = "#fb4934"
+
+myDarkGreen    = "#98971a"
+myLightGreen   = "#b8bb26"
+
+myDarkYellow   = "#d79921"
+myLightYellow  = "#fabd2f"
+
+myDarkBlue     = "#458588"
+myLightBlue    = "#83a598"
+
+myDarkMagenta  = "#b16286"
+myLightMagenta = "#d3869b"
+
+myDarkCyan     = "#689d6a"
+myLightCyan    = "#8ec07c"
+
+myDarkWhite    = "#a89984"
+myLightWhite   = "#ebdbb2"

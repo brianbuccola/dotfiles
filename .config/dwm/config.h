9ade068f9cb748eeae7c43112c20ff9b1d20a073
Brian Buccola <brian.buccola@gmail.com>
2017-11-29 22:20:21 +0100

    Add keybinding Mod-Esc -> mouse-warp

--- .config/dwm/config.h
+++ .config/dwm/config.h
@@ -70,6 +70,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_slash,  spawn,          {.v = manmenucmd } },
 	{ MODKEY,                       XK_p,      spawn,          {.v = passmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_Escape, spawn,          SHCMD("mouse-warp") },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },

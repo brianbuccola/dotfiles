8e74b58ec2710a8c191d4018a78b7e9822b1ed12
Brian Buccola <brian.buccola@gmail.com>
2018-02-07 21:40:04 +0100

    Add ControlMask for focusing prev/next monitor with ,/.

--- .config/dwm/config.h
+++ .config/dwm/config.h
@@ -89,8 +89,8 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_asterisk, view,         {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_asterisk, tag,          {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
+	{ MODKEY|ControlMask,           XK_comma,  focusmon,       {.i = -1 } },
+	{ MODKEY|ControlMask,           XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
 	TAGKEYS(                        XK_parenleft,              0)

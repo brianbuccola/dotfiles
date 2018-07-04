613ab162ad2f0f19a8c94e4d868c623222fb95e5
Brian Buccola <brian.buccola@gmail.com>
2018-07-04 12:29:59 +0200

    Split Gruvbox.hs into GruvboxDark.hs and GruvboxLight.hs

---
+++ .xmonad/lib/Colors/GruvboxDark.hs
@@ -0,0 +1,22 @@
+module Colors.GruvboxDark where
+
+-- ================================
+--  Gruvbox-inspired colors (dark)
+-- ================================
+
+myBlack         = "#000000"
+myRed           = "#cc241d"
+myGreen         = "#98971a"
+myYellow        = "#d79921"
+myBlue          = "#458588"
+myMagenta       = "#b16286"
+myCyan          = "#689d6a"
+myWhite         = "#a89984"
+myBrightBlack   = "#928374"
+myBrightRed     = "#fb4934"
+myBrightGreen   = "#b8bb26"
+myBrightYellow  = "#fabd2f"
+myBrightBlue    = "#83a598"
+myBrightMagenta = "#d3869b"
+myBrightCyan    = "#8ec07c"
+myBrightWhite   = "#ebdbb2"
---
+++ .xmonad/lib/Colors/GruvboxLight.hs
@@ -0,0 +1,22 @@
+module Colors.GruvboxLight where
+
+-- =================================
+--  Gruvbox-inspired colors (light)
+-- =================================
+
+myBlack         = "#fbf1c7"
+myRed           = "#cc241d"
+myGreen         = "#98971a"
+myYellow        = "#d79921"
+myBlue          = "#458588"
+myMagenta       = "#b16286"
+myCyan          = "#689d6a"
+myWhite         = "#7c6f64"
+myBrightBlack   = "#928374"
+myBrightRed     = "#9d0006"
+myBrightGreen   = "#79740e"
+myBrightYellow  = "#b57614"
+myBrightBlue    = "#076678"
+myBrightMagenta = "#8f3f71"
+myBrightCyan    = "#427b58"
+myBrightWhite   = "#3c3836"

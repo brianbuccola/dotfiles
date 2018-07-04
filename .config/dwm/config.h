b526a02b698c1b1bf1ad564488a258450ba43bf7
Brian Buccola <brian.buccola@gmail.com>
2018-07-04 12:37:30 +0200

    Move comment line

--- .config/dwm/config.h
+++ .config/dwm/config.h
@@ -14,9 +14,9 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-/* gruvbox-dark colors */
 static const char *fonts[]          = { "xos4 Terminus:size=9" };
 static const char dmenufont[]       = "xos4 Terminus:size=9";
+/* gruvbox-dark colors */
 static const char black[]           = "#000000";
 static const char red[]             = "#cc241d";
 static const char green[]           = "#98971a";

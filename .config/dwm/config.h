b6ae47fc6c7e5ba41884eda36a07038e67033583
Brian Buccola <brian.buccola@gmail.com>
2018-07-06 09:12:25 +0200

    Use Terminess (Nerd Font) as fallback

--- .config/dwm/config.h
+++ .config/dwm/config.h
@@ -14,7 +14,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "xos4 Terminus:size=9" };
+static const char *fonts[]          = { "xos4 Terminus:size=9", "TerminessTTF Nerd Font:size=9" };
 static const char dmenufont[]       = "xos4 Terminus:size=9";
 /* gruvbox-dark colors */
 static const char black[]           = "#000000";

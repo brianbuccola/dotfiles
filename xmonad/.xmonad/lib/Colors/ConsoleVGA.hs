4055962ac71c28243576341c950e51d05a20a37b
Brian Buccola <brian.buccola@gmail.com>
2018-11-04 10:20:46 +0100

    Commit new files

---
+++ xmonad/.xmonad/lib/Colors/ConsoleVGA.hs
@@ -0,0 +1,29 @@
+module Colors.ConsoleVGA where
+
+-- ============================
+--  ConsoleVGA-inspired colors
+-- ============================
+
+myDarkBlack    = "#000000"
+myLightBlack   = "#555555"
+
+myDarkRed      = "#aa0000"
+myLightRed     = "#ff5555"
+
+myDarkGreen    = "#00aa00"
+myLightGreen   = "#55ff55"
+
+myDarkYellow   = "#aa5500"
+myLightYellow  = "#ffff55"
+
+myDarkBlue     = "#0000aa"
+myLightBlue    = "#5555ff"
+
+myDarkMagenta  = "#aa00aa"
+myLightMagenta = "#ff55ff"
+
+myDarkCyan     = "#00aaaa"
+myLightCyan    = "#55ffff"
+
+myDarkWhite    = "#aaaaaa"
+myLightWhite   = "#ffffff"
---
+++ xmonad/.xmonad/lib/Colors/Molokai.hs
@@ -0,0 +1,29 @@
+module Colors.Molokai where
+
+-- =========================
+--  Molokai-inspired colors
+-- =========================
+
+myDarkBlack    = "#000000"
+myLightBlack   = "#1c1c1c"
+
+myDarkRed      = "#960050"
+myLightRed     = "#ff0090"
+
+myDarkGreen    = "#66aa11"
+myLightGreen   = "#80ff00"
+
+myDarkYellow   = "#c47f2c"
+myLightYellow  = "#f8ba86"
+
+myDarkBlue     = "#30309b"
+myLightBlue    = "#5f5fee"
+
+myDarkMagenta  = "#7e40a5"
+myLightMagenta = "#bb88dd"
+
+myDarkCyan     = "#3579a8"
+myLightCyan    = "#4eb4fa"
+
+myDarkWhite    = "#5f5f5f"
+myLightWhite   = "#d0d0d0"

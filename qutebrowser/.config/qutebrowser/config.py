fb673ded9a5ba1d707aab172d27844e20d5033c6
Brian Buccola <brian.buccola@gmail.com>
2019-10-26 08:40:21 -0400

    Remove nop bindings (issue #4579) after Qt upgrade fix

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -116,13 +116,6 @@ config.bind(';u', "spawn --userscript open-doi", mode="normal")
 config.bind(';p', "hint links run :open -p {hint-url}")
 config.bind(';m', "hint links spawn st -e mutt {hint-url}", mode="normal")
 
-## workaround for https://github.com/qutebrowser/qutebrowser/issues/4579
-config.bind('<tab>', "nop", mode="normal")
-config.bind('<tab>', "nop", mode="hint")
-config.bind('<tab>', "nop", mode="passthrough")
-config.bind('<tab>', "nop", mode="caret")
-config.bind('<tab>', "nop", mode="register")
-
 ## command mode
 config.bind('<ctrl-e>', "edit-command --run", mode="command")
 

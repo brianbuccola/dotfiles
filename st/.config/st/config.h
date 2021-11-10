e48a740c25a9b0ebb76f7afae06b9beb97931318
Brian Buccola <brian.buccola@gmail.com>
2021-11-10 10:36:12 -0500

    Simplify font: remove antialias=true:autohint=true

--- st/.config/st/config.h
+++ st/.config/st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Fira Mono:size=13:antialias=true:autohint=true";
+static char *font = "Fira Mono:size=13";
 static int borderpx = 2;
 
 /*

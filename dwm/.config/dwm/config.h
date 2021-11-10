9dacb2523bc56f27c929044b474ae3dff745b580
Brian Buccola <brian.buccola@gmail.com>
2021-11-10 10:35:38 -0500

    Increase Dina font size: 10 -> 12

--- dwm/.config/dwm/config.h
+++ dwm/.config/dwm/config.h
@@ -15,8 +15,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Dina:size=10" };
-static const char dmenufont[]       = "Dina:size=10";
+static const char *fonts[]          = { "Dina:size=12" };
+static const char dmenufont[]       = "Dina:size=12";
 /* gruvbox-dark colors */
 static const char black[]           = "#000000";
 static const char red[]             = "#cc241d";
